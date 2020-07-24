6725c3a16b0577c63ed4304549cc1e5ba9b074c7
zhq <316668064@qq.com>
2020-07-24 11:44:39 +0800

    update userController

--- controller/UserController.java
+++ controller/UserController.java
@@ -1,9 +1,14 @@
-package com.ecdata.demo.controller;
-import com.ecdata.demo.pojo.User;
-import com.ecdata.demo.service.UserService;
+package com.example.demo.controller;
+
+import com.example.demo.pojo.User;
+import com.example.demo.pojo.UserExample;
+import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -14,4 +19,33 @@ public class UserController {
     public int add(User user) {
         return userService.add(user);
     }
+
+    @GetMapping("/deleteById")
+    public int deleteByPrimaryKey(Long userId) {
+        return userService.deleteByPrimaryKey(userId);
+    }
+    @GetMapping("/deleteByExample")
+    public int deleteByExample(UserExample example) {
+        return userService.deleteByExample(example);
+    }
+
+    @PostMapping("/updateById")
+    public int updateByPrimaryKey(User user) {
+        return userService.updateByPrimaryKey(user);
+    }
+    @PostMapping("/updateByExample")
+    public int updateByExample(User record, UserExample example) {
+        return userService.updateByExample(record,example);
+    }
+
+    @GetMapping("/selectByExample")
+    public List<User> selectByExample(UserExample example) {
+        return userService.selectByExample(example);
+    }
+    @GetMapping("/selectByPrimaryKey")
+    public User selectByPrimaryKey(Long userId) {
+        return userService.selectByPrimaryKey(userId);
+    }
+
+
 }
